fd2928ca8de16b0568ce38ef928389cb25208603
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-06-28 22:43:26 +0800

    chore: upgrade deps

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -4,7 +4,6 @@ import IsolatedDecl from './src/rolldown'
 export default defineConfig({
   entry: ['./src/*.ts'],
   format: ['cjs', 'esm'],
-  splitting: true,
   clean: true,
   plugins: [IsolatedDecl()],
 })
