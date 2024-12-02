4aa283e13ce11168aedf38c2b9fb0c63abe93051
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-12-02 08:57:35 +0800

    test: improve file snapshot

--- tests/_utils.ts
+++ tests/_utils.ts
@@ -1,25 +1,26 @@
-import { readdir, readFile } from 'node:fs/promises'
+import { readFile } from 'node:fs/promises'
 import path from 'node:path'
+import { glob } from 'tinyglobby'
 
-export async function getFileSnapshot(dir: string): Promise<string[]> {
+export async function getFileSnapshot(
+  dir: string,
+  pattern = '**/*',
+): Promise<string> {
   /**
    * Map written output from file system rather than from bundle due to
    * module execution order not consistent
    *
    * @see https://github.com/rollup/rollup/issues/3888
    */
-  const files = (
-    await readdir(dir, { recursive: true, withFileTypes: true })
-  ).filter((it) => it.isFile())
-
+  const files = await glob(pattern, { cwd: dir })
   const snapshot = await Promise.all(
     files.map(async (it) => {
-      const absolute = path.resolve(it.parentPath, it.name)
+      const absolute = path.resolve(dir, it)
       const filePath = path.relative(dir, absolute)
       const content = await readFile(absolute, 'utf-8')
 
       return `// ${filePath.replaceAll('\\', '/')}\n${content.toString()}`
     }),
   )
-  return snapshot
+  return snapshot.sort().join('\n\n')
 }
--- vitest.config.ts
+++ vitest.config.ts
@@ -3,10 +3,5 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     setupFiles: 'vitest-setup.ts',
-    server: {
-      deps: {
-        external: [/node_modules/],
-      },
-    },
   },
 })
